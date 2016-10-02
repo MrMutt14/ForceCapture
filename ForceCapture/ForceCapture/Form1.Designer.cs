155582c4ddb7fb175a71d8b4b64ebcb0335cf247
MrMutt14 <MrMutt14@users.noreply.github.com>
2016-10-03 00:38:32 +0300

    first

---
+++ ForceCapture/ForceCapture/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace ForceCapture
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSave = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(59, 23);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(97, 32);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Capture";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click_1);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "bmp";
+            this.saveFileDialog1.Filter = "BMP|*.bmp|PNG|*.png|JPEG|*.jpg";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(225, 79);
+            this.Controls.Add(this.btnSave);
+            this.Name = "Form1";
+            this.Text = "ForceCapture";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ ForceCapture/ForceCapture/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Drawing.Imaging;
+
+namespace ForceCapture
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ImageFormat img;
+        Bitmap bt;
+        Graphics screenShot;
+      
+        
+
+        private void btnSave_Click_1(object sender, EventArgs e)
+        {
+            
+            
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.Hide();
+                Thread.Sleep(500);
+                bt = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height, PixelFormat.Format32bppArgb);
+                screenShot = Graphics.FromImage(bt);
+                screenShot.CopyFromScreen(Screen.PrimaryScreen.Bounds.X, Screen.PrimaryScreen.Bounds.Y, 0, 0, Screen.PrimaryScreen.Bounds.Size, CopyPixelOperation.SourceCopy);
+                switch (saveFileDialog1.FilterIndex)
+                {
+                    case 0: img = ImageFormat.Bmp; break;
+                    case 1: img = ImageFormat.Png; break;
+                    case 2: img = ImageFormat.Jpeg; break;
+                }
+                bt.Save(saveFileDialog1.FileName, img);
+                this.Show();
+            }
+        }
+
+       
+    }
+}
